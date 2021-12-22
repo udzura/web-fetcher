5c6af9b4d695ba323f9c88f4b9ee0d9f5f25fb86
Uchio Kondo <udzura@udzura.jp>
2021-12-22 19:44:18 +0900

    Implement case 2

--- lib/web_fetcher.rb
+++ lib/web_fetcher.rb
@@ -3,6 +3,7 @@
 require_relative "web_fetcher/version"
 require 'uri'
 require 'net/http'
+require 'nokogiri'
 
 module WebFetcher
   class Error < StandardError
@@ -13,16 +14,26 @@ module WebFetcher
       @path_rule = lambda {|uri|
         "#{uri.host}.html"
       }
+      @metadata = nil
     end
 
     def process(target)
+      @metadata = Metadata.new(target)
+      @metadata.set_last_fetch!(@path_rule.call(target))
+
       content = get(target)
+      @metadata.parse_body_and_set_metadata!(content)
+
+      print_metadata @metadata
+
       dir = Dir.pwd
       output_to_file(content, dir, @path_rule.call(target))
     end
 
     private
     def get(target)
+
+
       http = Net::HTTP.new(target.host, target.port)
       if target.scheme == 'https'
         http.use_ssl = true
@@ -42,6 +53,41 @@ module WebFetcher
       File.open(File.join(dir, path), 'w') do |f|
         f.write content
       end
+    end
+
+    def print_metadata(metadata)
+      puts <<~DATA
+        site: #{metadata.site}
+        num_links: #{metadata.num_links}
+        images: #{metadata.images}
+        last_fetch: #{metadata.last_fetch || "<not yet>"}
+      DATA
+    end
+  end
+
+  class Metadata
+    def initialize(uri)
+      @site = uri.host
+      @num_links = 0
+      @images = 0
+    end
+    attr_reader :site, :num_links, :images, :last_fetch
+
+    def set_last_fetch!(filepath)
+      if File.exist?(filepath)
+        @last_fetch = File.stat(filepath).mtime
+      else
+        @last_fetch = nil
+      end
+    end
+
+    def parse_body_and_set_metadata!(body)
+      doc = Nokogiri::HTML body
+      @num_links = doc.css('a').size
+      @images = doc.css('img').size
+    rescue => e
+      # if failed ro parse XML, fallback to regex...
+      # but TBA
     end
   end
 
