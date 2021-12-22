e971273958d39cb8ec845f15653bda654aad868f
Uchio Kondo <udzura@udzura.jp>
2021-12-22 18:48:18 +0900

    Green

--- test/smoke_test.rb
+++ test/smoke_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class WebFetcherSmokeTest < Test::Unit::TestCase
   def test_version
-    assert_equal("1.0.0", WebFetcher::VERSION)
+    assert_equal("0.1.0", WebFetcher::VERSION)
   end
 end
